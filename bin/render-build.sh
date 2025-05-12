0bbf0178fe5708ea794ce4acbd368796e8febb5f
onomusya <onomusya@gmail.com>
2025-05-12 18:39:31 +0900

    デプロイ

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:migrate --trace
 # bundle exec rails db:seed ← 一旦コメントアウト！
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_09_062947) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_09_061356) do
   create_table "reservations", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "site_id", null: false
